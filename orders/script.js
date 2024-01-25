afb94c32b0032518b14a5195337e3d3b65253a7d
pouria <pouria.sameti80@gmail.com>
2024-01-25 18:40:51 +0330

    orders page -> adding functionality for showing and closing previous-order-detail.

--- orders/script.js
+++ orders/script.js
@@ -3,6 +3,8 @@
 function show_order_trac(){
 
     let order_trac = document.getElementsByClassName('order-trac-detail')[0];
+    let page = document.getElementsByClassName("all-page")[0];
+    page.style.backgroundColor = "#00000029";
     order_trac.style.display = 'flex';
 }
 
@@ -10,5 +12,24 @@ function show_order_trac(){
 function close_order_trac(){
 
     let order_trac = document.getElementsByClassName('order-trac-detail')[0];
+    let page = document.getElementsByClassName("all-page")[0];
+    page.style.backgroundColor = "#FFFFFF";
     order_trac.style.display = 'none';
+}
+
+
+function show_previous_order_detail(){
+    let previous_order_trac = document.getElementsByClassName('previous-order-detail')[0];
+    let page = document.getElementsByClassName("all-page")[0];
+    page.style.backgroundColor = "#00000029";
+    previous_order_trac.style.display = 'flex';
+
+}
+
+
+function close_previous_order_detail(){
+    let previous_order_trac = document.getElementsByClassName('previous-order-detail')[0];
+    let page = document.getElementsByClassName("all-page")[0];
+    page.style.backgroundColor = "#FFFFFF";
+    previous_order_trac.style.display = 'none';
 }
