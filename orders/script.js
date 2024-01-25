afb013a400329bab3c5291c31f7041a5139ff009
pouria <pouria.sameti80@gmail.com>
2024-01-25 17:13:07 +0330

    orders page -> add functionality for showing order-trac with JS.

---
+++ orders/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function show_order_trac(){
+
+    let order_trac = document.getElementsByClassName('order-trac-detail')[0];
+    order_trac.style.display = 'flex';
+}
+
+
+function close_order_trac(){
+
+    let order_trac = document.getElementsByClassName('order-trac-detail')[0];
+    order_trac.style.display = 'none';
+}
