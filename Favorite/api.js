c40f6729fecf1380fc2dcb05db5fd873e09da571
Erfan Samimi <erf80sami@gmail.com>
2024-01-26 17:20:23 +0330

    Add remove an item from favorite foods

--- Favorite/api.js
+++ Favorite/api.js
@@ -1,6 +1,6 @@
 
-function CreateRestaurantItem(id,name, score, rating_counts, category, image_src){
-    item = `          
+function CreateRestaurantItem(id, name, score, rating_counts, category, image_src) {
+  item = `          
     <div class="restaurant-snapshot">
     <div class="rest-img">
       <img src="http://127.0.0.1:8000${image_src}" alt="${name} restaurant" />
@@ -30,19 +30,45 @@ function CreateRestaurantItem(id,name, score, rating_counts, category, image_src
 }
 
 
+function removeFoodFromFavorties(id) {
+  const accessToken = localStorage.getItem('access_token');
 
+  if (!accessToken) {
+    console.error('Access token not found in local storage');
+    return;
+  }
 
-function CreateFoodItem(id,name, delivery_pirce, image_src, score, rating_counts, category, min_delivery_time, max_delivery_time){
-    if (delivery_pirce == 0){
-        delivery_pirce = "Free";
-    }
-    else{
-        delivery_pirce = `$${delivery_pirce}`;
-    }
+  const FavoriteFoodDeleteUrl = `http://127.0.0.1:8000/api/restaurants/favorite/food/${id}`;
 
-    var delivery_time = max_delivery_time - min_delivery_time;
+  fetch(FavoriteFoodDeleteUrl, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    method:'DELETE'
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      else{
+        UpdateFavoriteFoodList();
+      }
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
 
-    item = `          
+function CreateFoodItem(id, name, delivery_pirce, image_src, score, rating_counts, category, min_delivery_time, max_delivery_time) {
+  if (delivery_pirce == 0) {
+    delivery_pirce = "Free";
+  }
+  else {
+    delivery_pirce = `$${delivery_pirce}`;
+  }
+
+  var delivery_time = max_delivery_time - min_delivery_time;
+
+  item = `          
     <div class="food-snapshot">
     <div class="food-image">
       <img src="http://127.0.0.1:8000${image_src}" alt="${name} image" />
@@ -69,12 +95,66 @@ function CreateFoodItem(id,name, delivery_pirce, image_src, score, rating_counts
         <img src="icons/delivery.svg" alt="price group" />
         <p>&nbsp;${delivery_time} min</p>
       </div>
+      <div class="food-favorite-btn">
+                <button type="button">
+                  <img
+                    src="icons/favorite-heart-button.png"
+                    alt="favorite food btn",
+                    onclick="removeFoodFromFavorties(${id})"
+                  />like
+                </button>
+              </div>
     </div>
   </div>
   `
   return item;
 }
 
+
+function UpdateFavoriteFoodList() {
+  const accessToken = localStorage.getItem('access_token');
+
+  if (!accessToken) {
+    console.error('Access token not found in local storage');
+    return;
+  }
+
+
+  const FavoriteFoodListUrl = 'http://127.0.0.1:8000/api/restaurants/favorite/food/';
+
+  fetch(FavoriteFoodListUrl, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      document.querySelector('.dishes-favorite').innerHTML = "";
+      if (data && data.length > 0) {
+        data.forEach(item => {
+          const htmlItem = CreateFoodItem(
+            item.id,
+            item.name,
+            item.delivery_price,
+            item.image,
+            item.score,
+            item.ratings_count,
+            item.category.name,
+            item.min_delivery_time,
+            item.max_delivery_time,
+          );
+          document.querySelector('.dishes-favorite').innerHTML += htmlItem;
+        });
+      }
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -118,41 +198,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-
-  const AsianListUrl = 'http://127.0.0.1:8000/api/restaurants/foods/list?sort_by_score=-1&category_name=Asian&skip=0&limit=3';
-
-  fetch(AsianListUrl, {
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data && data.length > 0) {
-        data.forEach(item => {
-          const htmlItem = CreateFoodItem(
-            item.id,
-            item.name,
-            item.delivery_price,
-            item.image,
-            item.score,
-            item.ratings_count,
-            item.category.name,
-            item.min_delivery_time,
-            item.max_delivery_time,
-          );
-          document.querySelector('.dishes-favorite').innerHTML += htmlItem;
-        });
-      }
-    })
-    .catch(error => console.error('Error fetching data:', error));
-
-
-  }
-  );
+    UpdateFavoriteFoodList();
+}
+);
