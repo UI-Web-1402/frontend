08c875182a4836b04ee4cf864ba48d3fb74c672e
pouria <pouria.sameti80@gmail.com>
2024-01-26 11:48:00 +0330

    settings page -> adding functionality for personal edit to show & hide with JS.

--- settings/script.js
+++ settings/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+
+function close_personal_settings(){
+    let p_settings = document.getElementsByClassName('personal-edit')[0];
+    p_settings.style.display = 'none';
+}
+
+
+function show_personal_settings(){
+    let p_settings = document.getElementsByClassName('personal-edit')[0];
+    p_settings.style.display = 'flex';
+}
