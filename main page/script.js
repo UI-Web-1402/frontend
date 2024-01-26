b11b8266730420f320c8282a9f8190628eabf05a
pouria <pouria.sameti80@gmail.com>
2024-01-26 19:15:39 +0330

    main page -> cart menu showing & hiding manages with JS.

--- main page/script.js
+++ main page/script.js
@@ -9,3 +9,13 @@ function show_cart_part(){
     let cart_part = document.getElementsByClassName('cart-part')[0];
     cart_part.style.display = "flex";
 }
+
+function close_confirm_part(){
+    let cart_part = document.getElementsByClassName('confirm-address')[0];
+    cart_part.style.display = "none";
+}
+
+function show_confirm_part(){
+    let cart_part = document.getElementsByClassName('confirm-address')[0];
+    cart_part.style.display = "flex";
+}
