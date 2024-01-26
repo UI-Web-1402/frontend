5452e54f704732dd1ff88be38bd1350b9550f4aa
pouria <pouria.sameti80@gmail.com>
2024-01-26 12:18:14 +0330

    settings page -> adding functionality for showing & hiding saved address edit part.

--- settings/script.js
+++ settings/script.js
@@ -11,3 +11,13 @@ function show_personal_settings(){
     let p_settings = document.getElementsByClassName('personal-edit')[0];
     p_settings.style.display = 'flex';
 }
+
+function close_saved_address_settings(){
+    let saved_address_settings = document.getElementsByClassName('saved-address-edit')[0];
+    saved_address_settings.style.display = 'none';
+}
+
+function show_saved_address_settings(){
+    let saved_address_settings = document.getElementsByClassName('saved-address-edit')[0];
+    saved_address_settings.style.display = 'flex';
+}
