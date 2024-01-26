7a8b0791d751853a6cc9881b0da64cd17c369d4a
pouria <pouria.sameti80@gmail.com>
2024-01-26 19:58:34 +0330

    settings page -> adding cart-part & conform-address-part to page.

--- settings/script.js
+++ settings/script.js
@@ -21,3 +21,24 @@ function show_saved_address_settings(){
     let saved_address_settings = document.getElementsByClassName('saved-address-edit')[0];
     saved_address_settings.style.display = 'flex';
 }
+
+function close_cart_part(){
+    let cart_part = document.getElementsByClassName('cart-part')[0];
+    cart_part.style.display = "none";
+}
+
+function show_cart_part(){
+    let cart_part = document.getElementsByClassName('cart-part')[0];
+    cart_part.style.display = "flex";
+}
+
+
+function close_confirm_part(){
+    let cart_part = document.getElementsByClassName('confirm-address')[0];
+    cart_part.style.display = "none";
+}
+
+function show_confirm_part(){
+    let cart_part = document.getElementsByClassName('confirm-address')[0];
+    cart_part.style.display = "flex";
+}
