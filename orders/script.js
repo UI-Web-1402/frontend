3bf9f3732108e22147dce02a51fa42afeca4f77f
pouria <pouria.sameti80@gmail.com>
2024-01-26 19:44:32 +0330

    orders page -> adding cart-part & confirm-address part to the page.

--- orders/script.js
+++ orders/script.js
@@ -33,3 +33,24 @@ function close_previous_order_detail(){
     page.style.backgroundColor = "#FFFFFF";
     previous_order_trac.style.display = 'none';
 }
+
+function close_cart_part(){
+    let cart_part = document.getElementsByClassName('cart-part')[0];
+    cart_part.style.display = "none";
+}
+
+function show_cart_part(){
+    let cart_part = document.getElementsByClassName('cart-part')[0];
+    cart_part.style.display = "flex";
+}
+
+
+function close_confirm_part(){
+    let cart_part = document.getElementsByClassName('confirm-address')[0];
+    cart_part.style.display = "none";
+}
+
+function show_confirm_part(){
+    let cart_part = document.getElementsByClassName('confirm-address')[0];
+    cart_part.style.display = "flex";
+}
